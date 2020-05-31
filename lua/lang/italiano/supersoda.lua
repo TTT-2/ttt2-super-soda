728a0cb5ab9ce0fde5cec84d6633460c0326f92b
ThePlatynumGhost <theplatinumale@gmail.com>
2020-05-31 14:10:44 +0200

    Added Italian translation

---
+++ lua/lang/italiano/supersoda.lua
@@ -0,0 +1,33 @@
+L = LANG.GetLanguageTableReference("italiano")
+
+L["ttt_drank_soda_already_drunk"] = "Puoi bere ogni soda solo una volta."
+L["ttt_drank_soda_limit_reached"] = "Puoi bere solo una soda!"
+L["ttt_drank_soda_cant_pickup"] = "Non puoi bere o prendere niente in questo momento."
+L["ttt_drank_soda_unlimted"] = "Le sode con effetti instantanei possono essere bevute infinite volte."
+L["ttt_spawned_soda"] = "Ci sono {amount} super sode questo round. Bevile per avere dei potenziamenti!"
+
+L["soda_armorup"] = "ArmorUp!™"
+L["soda_creditup"] = "CreditUp!™"
+L["soda_healup"] = "HealUp!™"
+L["soda_jumpup"] = "JumpUp!™"
+L["soda_rageup"] = "RageUp!™"
+L["soda_shootup"] = "ShootUp!™"
+L["soda_speedup"] = "SpeedUp!™"
+
+L["ttt_drank_soda_speedup"] = "Hai bevuto la SpeedUp!™ ora puoi Moverti più velocemente!"
+L["ttt_drank_soda_rageup"] = "Hai bevuto la RageUp!™ ora fai più Danno!"
+L["ttt_drank_soda_shootup"] = "Hai bevuto la ShootUp!™ ora hai più Rateo di fuoco!"
+L["ttt_drank_soda_jumpup"] = "Hai beveuto la JumpUp!™ ora puoi fare il Salto doppio!"    
+L["ttt_drank_soda_armorup"] = "Hai bevuto la ArmorUp!™ ora hai punti Armatura in più!"
+L["ttt_drank_soda_healup"] = "Hai bevuto la HealUp!™ ora hai ricevuto HP aggiuntivi!"
+L["ttt_drank_soda_creditup"] = "Hai bevuto la CreditUp!™ ora hai ricevuto qualche Credito dello shop!"
+
+L["ttt_pickup_soda_speedup"] = "Ti di muoverai più velocemente dopo aver bevuto questa soda"
+L["ttt_pickup_soda_rageup"] = "Farai più danno dopo aver bevuto questa soda"
+L["ttt_pickup_soda_shootup"] = "Avrai più rateo di fuoco dopo aver bevuto questa soda"
+L["ttt_pickup_soda_jumpup"] = "Potrai fare il salto doppio dopo aver bevuto questa soda"
+L["ttt_pickup_soda_armorup"] = "Ricevi punti armatura aggiuntivi bevendo questa soda [INSTANT]"
+L["ttt_pickup_soda_healup"] = "Ricevi HP aggiuntivi bevendo questa soda [INSTANT]"
+L["ttt_pickup_soda_creditup"] = "Ricevi crediti aggiuntivi bevendo questa soda [INSTANT]"
+
+L["ttt_pickup_soda"] = "Premi [{usekey}] per bere la soda"
