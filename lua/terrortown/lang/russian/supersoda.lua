95e77cfdea874eba95334bf289505b33c9ba9121
Blueberryy <36592509+Blueberryy@users.noreply.github.com>
2020-10-06 17:06:47 +0500

    Create supersoda.lua (#5)

---
+++ lua/terrortown/lang/russian/supersoda.lua
@@ -0,0 +1,33 @@
+L = LANG.GetLanguageTableReference("Русский")
+
+L["ttt_drank_soda_already_drunk"] = "Выпить каждую газировку можно только один раз."
+L["ttt_drank_soda_limit_reached"] = "Можно пить только одну газировку!"
+L["ttt_drank_soda_cant_pickup"] = "Вы не можете ничего пить или подобрать прямо сейчас."
+L["ttt_drank_soda_unlimted"] = "Газированные напитки с мгновенным эффектом можно пить бесконечное количество раз."
+L["ttt_spawned_soda"] = "В этом раунде будет {amount} супер газированных напитков. Выпейте их, чтобы получить бонусы!"
+
+L["soda_armorup"] = "ArmorUp!™"
+L["soda_creditup"] = "CreditUp!™"
+L["soda_healup"] = "HealUp!™"
+L["soda_jumpup"] = "JumpUp!™"
+L["soda_rageup"] = "RageUp!™"
+L["soda_shootup"] = "ShootUp!™"
+L["soda_speedup"] = "SpeedUp!™"
+
+L["ttt_drank_soda_speedup"] = "Вы нашли банку SpeedUp!™ Двигайся быстрее!"
+L["ttt_drank_soda_rageup"] = "Вы нашли банку RageUp!™ Наноси больше урона!"
+L["ttt_drank_soda_shootup"] = "Вы нашли банку ShootUp!™ Стреляй быстрее!"
+L["ttt_drank_soda_jumpup"] = "Вы нашли банку JumpUp!™ Теперь вы можете прыгнуть дважды!"
+L["ttt_drank_soda_armorup"] = "Вы нашли банку ArmorUp!™ Вы получили дополнительные очки брони!"
+L["ttt_drank_soda_healup"] = "Вы нашли банку HealUp!™ Вы получили дополнительные очки здоровья!"
+L["ttt_drank_soda_creditup"] = "Вы нашли банку CreditUp!™ Вы получили дополнительные кредиты!"
+
+L["ttt_pickup_soda_speedup"] = "Вы будете двигаться быстрее после того, как выпьете эту газировку"
+L["ttt_pickup_soda_rageup"] = "Вы будете наносить больше урона, выпив эту газировку"
+L["ttt_pickup_soda_shootup"] = "Вы будете стрелять быстрее, выпив эту газировку"
+L["ttt_pickup_soda_jumpup"] = "Вы сможете дважды прыгнуть после того, как выпьете эту газировку"
+L["ttt_pickup_soda_armorup"] = "Вы получите дополнительные очки брони, выпив эту газировку [МГНОВЕННЫЙ]"
+L["ttt_pickup_soda_healup"] = "Вы получите дополнительные очки здоровья, выпив эту газировку [МГНОВЕННЫЙ]"
+L["ttt_pickup_soda_creditup"] = "Вы получите дополнительные кредиты, выпив эту газировку [МГНОВЕННЫЙ]"
+
+L["ttt_pickup_soda"] = "Нажмите [{usekey}], чтобы выпить газировку"
