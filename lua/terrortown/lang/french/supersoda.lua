c0d147acbc84ac6c3b6d1d0eb717d722413c1fc3
MisterClems <73431903+MisterClems@users.noreply.github.com>
2020-11-01 03:00:13 +0100

    Create supersoda.lua
    french translation :supersoda

---
+++ lua/terrortown/lang/french/supersoda.lua
@@ -0,0 +1,33 @@
+L = LANG.GetLanguageTableReference("french")
+
+L["ttt_drank_soda_already_drunk"] = "Vous pouvez boire chaque soda qu'une seule fois."
+L["ttt_drank_soda_limit_reached"] = "Vous pouvez boire qu'un seul soda!"
+L["ttt_drank_soda_cant_pickup"] = "Vous ne pouvez pas boire ou ramasser quoi que ce soit pour le moment."
+L["ttt_drank_soda_unlimted"] = "Le soda aux effets instantanés peut être bu à l'infini"
+L["ttt_spawned_soda"] = "Il y a {amount} super sodas ce round. Ramassez-les pour obtenir des avantages!"
+
+L["soda_armorup"] = "ArmorUp!™"
+L["soda_creditup"] = "CreditUp!™"
+L["soda_healup"] = "HealUp!™"
+L["soda_jumpup"] = "JumpUp!™"
+L["soda_rageup"] = "RageUp!™"
+L["soda_shootup"] = "ShootUp!™"
+L["soda_speedup"] = "SpeedUp!™"
+
+L["ttt_drank_soda_speedup"] = "Vous avez trouvé la cannette SpeedUp!™ ! Courrez plus vite!"
+L["ttt_drank_soda_rageup"] = "Vous avez trouvé la cannette RageUp!™ ! Faites plus de dégâts!"
+L["ttt_drank_soda_shootup"] = "Vous avez trouvé la cannette ShootUp!™ ! Tirez plus vite!"
+L["ttt_drank_soda_jumpup"] = "Vous avez trouvé la cannette JumpUp!™ ! Vous pouvez sauter deux fois!"
+L["ttt_drank_soda_armorup"] = "Vous avez trouvé la cannette ArmorUp!™ ! Vous avez reçu des points d'armure supplémentaires!"
+L["ttt_drank_soda_healup"] = "Vous avez trouvé la cannette HealUp!™ ! Vous avez reçu des points de vie supplémentaires!"
+L["ttt_drank_soda_creditup"] = "Vous avez trouvé la cannette CreditUp!™ ! Vous avez reçu des crédits d'équipement supplémentaires!"
+
+L["ttt_pickup_soda_speedup"] = "Vous irez plus vite après avoir bu ce soda"
+L["ttt_pickup_soda_rageup"] = "Vous ferez plus de dégâts après avoir bu ce soda"
+L["ttt_pickup_soda_shootup"] = "Vous tirerez plus vite après avoir bu ce soda"
+L["ttt_pickup_soda_jumpup"] = "Vous pouvez sauter deux fois après avoir bu ce soda"
+L["ttt_pickup_soda_armorup"] = "Vous recevez des points d'armure supplémentaires en buvant ce soda [INSTANT]"
+L["ttt_pickup_soda_healup"] = "Vous recevez des points de vie supplémentaires en buvant ce soda [INSTANT]"
+L["ttt_pickup_soda_creditup"] = "Vous recevez des crédits d'équipement supplémentaires en buvant ce soda [INSTANT]"
+
+L["ttt_pickup_soda"] = "Appuyez sur [{usekey}] pour boire du soda"
